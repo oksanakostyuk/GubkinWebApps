ec8607f5cc691bda31ea5e6c2ce8c95818543aaa
NVolkov <alarmwolf@gmail.com>
2014-10-17 12:21:47 +0400

    First Commit

---
+++ JavaIntroduction/JavaIntroduction/src/javaintroduction/JavaIntroduction.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintroduction;
+
+/**
+ *
+ * @author smith
+ */
+public class JavaIntroduction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int x = 4;
+        int y = 5;
+        System.out.println("x + y = " +(x+y));
+        
+       
+        
+    }
+}
